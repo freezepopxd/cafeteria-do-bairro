6fd4f985717ca0f6133e5789f20f7e7e452620cc
freezepopxd <luiz-altino@bol.com.br>
2025-04-13 10:34:24 -0400

    Primeiro commit: Criando a interface da Cafeteria do Bairro

---
+++ script.js
@@ -0,0 +1,4 @@
+// Exemplo de interatividade simples, como exibir um alerta ao clicar no botão
+document.querySelector('.btn-custom').addEventListener('click', function() {
+    alert('Pedido realizado com sucesso!');
+});
